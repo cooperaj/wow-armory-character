9f6b5418335fab5c39ac3d6696126a5c9c126a0e
Adam Cooper <adam@networkpie.co.uk>
2014-11-05 13:32:59 +0000

    Fix the instance of single talent characters having a second broken talent showing. Fixes #21.

--- view-wow-armory-character.php
+++ view-wow-armory-character.php
@@ -27,11 +27,14 @@
             <?php if ($options['show_talents'] == 1 && $character->talents) : ?>
                 <span class="armory_char_spec">
                 <?php $count = 0;
-                    foreach ($character->talents as $talent) : ?>
+                    foreach ($character->talents as $talent) :
+	                    if ($talent->calcSpec != "") : ?>
                     <span class="<?php echo (isset($talent->selected) && $talent->selected) ? 'active' : 'inactive'; ?>_spec">
                                     <a href="<?php echo $this->get_talent_url($talent); ?>"><img src="<?php echo $this->get_talent_tree_icon_url($talent); ?>" alt="<?php echo $talent->spec->name; ?> talent spec icon" /> <?php echo $this->get_talent_tree_text($talent); ?></a>
                                 </span>
-                <?php $count++;
+
+                <?php endif;
+		                $count++;
                     endforeach; ?>
                     <br />
                 </span>
